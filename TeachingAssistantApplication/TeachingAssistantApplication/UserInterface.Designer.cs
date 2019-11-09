88129ca7c59b375381d5026755118ba7b18e8f2a
Sam Hoggatt <schoggatt@ksu.edu>
2019-11-08 18:57:07 -0600

    ve 0.0.4

--- TeachingAssistantApplication/TeachingAssistantApplication/UserInterface.Designer.cs
+++ TeachingAssistantApplication/TeachingAssistantApplication/UserInterface.Designer.cs
@@ -28,81 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.components = new System.ComponentModel.Container();
+            this.uxTextBox = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.uxFriendIP = new System.Windows.Forms.TextBox();
+            this.uxLocalIP = new System.Windows.Forms.TextBox();
+            this.uxLocalPort = new System.Windows.Forms.TextBox();
+            this.uxFriendPort = new System.Windows.Forms.TextBox();
+            this.uxInputBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.uxSend = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // uxTextBox
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 192);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBox1.Size = new System.Drawing.Size(584, 414);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(1060, 612);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 44);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Next";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(927, 612);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(127, 44);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Previous";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // richTextBox2
-            // 
-            this.richTextBox2.Location = new System.Drawing.Point(602, 192);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBox2.Size = new System.Drawing.Size(584, 414);
-            this.richTextBox2.TabIndex = 3;
-            this.richTextBox2.Text = "";
+            this.uxTextBox.Location = new System.Drawing.Point(12, 141);
+            this.uxTextBox.Name = "uxTextBox";
+            this.uxTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.uxTextBox.Size = new System.Drawing.Size(655, 414);
+            this.uxTextBox.TabIndex = 0;
+            this.uxTextBox.Text = "";
+            this.uxTextBox.TextChanged += new System.EventHandler(this.UxQuestionBox_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 172);
+            this.label1.Location = new System.Drawing.Point(12, 121);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(69, 17);
             this.label1.TabIndex = 4;
             this.label1.Text = "Question:";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
             // 
-            // label2
+            // contextMenuStrip1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(602, 172);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 17);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Response:";
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.uxLocalPort);
+            this.groupBox1.Controls.Add(this.uxLocalIP);
+            this.groupBox1.Location = new System.Drawing.Point(15, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(192, 92);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Local";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.uxFriendPort);
+            this.groupBox2.Controls.Add(this.uxFriendIP);
+            this.groupBox2.Location = new System.Drawing.Point(213, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(192, 92);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Remote";
+            // 
+            // uxFriendIP
+            // 
+            this.uxFriendIP.Location = new System.Drawing.Point(86, 21);
+            this.uxFriendIP.Name = "uxFriendIP";
+            this.uxFriendIP.Size = new System.Drawing.Size(100, 22);
+            this.uxFriendIP.TabIndex = 0;
+            // 
+            // uxLocalIP
+            // 
+            this.uxLocalIP.Location = new System.Drawing.Point(86, 21);
+            this.uxLocalIP.Name = "uxLocalIP";
+            this.uxLocalIP.Size = new System.Drawing.Size(100, 22);
+            this.uxLocalIP.TabIndex = 1;
+            // 
+            // uxLocalPort
+            // 
+            this.uxLocalPort.Location = new System.Drawing.Point(86, 49);
+            this.uxLocalPort.Name = "uxLocalPort";
+            this.uxLocalPort.Size = new System.Drawing.Size(100, 22);
+            this.uxLocalPort.TabIndex = 2;
+            // 
+            // uxFriendPort
+            // 
+            this.uxFriendPort.Location = new System.Drawing.Point(86, 49);
+            this.uxFriendPort.Name = "uxFriendPort";
+            this.uxFriendPort.Size = new System.Drawing.Size(100, 22);
+            this.uxFriendPort.TabIndex = 3;
+            // 
+            // uxInputBox
+            // 
+            this.uxInputBox.Location = new System.Drawing.Point(12, 561);
+            this.uxInputBox.Name = "uxInputBox";
+            this.uxInputBox.Size = new System.Drawing.Size(522, 22);
+            this.uxInputBox.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(540, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 44);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // uxSend
+            // 
+            this.uxSend.Location = new System.Drawing.Point(540, 561);
+            this.uxSend.Name = "uxSend";
+            this.uxSend.Size = new System.Drawing.Size(127, 44);
+            this.uxSend.TabIndex = 13;
+            this.uxSend.Text = "Send";
+            this.uxSend.UseVisualStyleBackColor = true;
+            this.uxSend.Click += new System.EventHandler(this.UxSend_Click);
             // 
             // UserInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1199, 668);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(687, 668);
+            this.Controls.Add(this.uxSend);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.uxInputBox);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.uxTextBox);
             this.Name = "UserInterface";
             this.Text = "Quick Question";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,12 +173,18 @@
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox uxTextBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox uxLocalPort;
+        private System.Windows.Forms.TextBox uxLocalIP;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox uxFriendPort;
+        private System.Windows.Forms.TextBox uxFriendIP;
+        private System.Windows.Forms.TextBox uxInputBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button uxSend;
     }
 }
 
--- TeachingAssistantApplication/TeachingAssistantApplication/UserInterface.cs
+++ TeachingAssistantApplication/TeachingAssistantApplication/UserInterface.cs
@@ -8,13 +8,117 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Net;
+using System.Net.Sockets;
+
 namespace TeachingAssistantApplication
 {
     public partial class UserInterface : Form
     {
+        Socket sck;
+        EndPoint epLocal, epRemote;
         public UserInterface()
         {
             InitializeComponent();
+
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            uxLocalIP.Text = GetLocalIP();
+            uxFriendIP.Text = GetLocalIP();
+        }
+
+        private string GetLocalIP()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach(IPAddress ip in host.AddressList)
+            {
+                if(ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return "127.0.0.1";
+        }
+
+        private void MessageCallBack(IAsyncResult aResult)
+        {
+            try
+            {
+                int size = sck.EndReceiveFrom(aResult, ref epRemote);
+                if(size > 0)
+                {
+                    byte[] receivedData = new byte[1464];
+                    receivedData = (byte[])aResult.AsyncState;
+
+                    ASCIIEncoding eEncoding = new ASCIIEncoding();
+                    string receivedMessage = eEncoding.GetString(receivedData);
+
+                    uxTextBox.Text += "\nTest: " + receivedMessage;
+                }
+
+                byte[] buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UxQuestionBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Start event handler
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                epLocal = new IPEndPoint(IPAddress.Parse(uxLocalIP.Text), Convert.ToInt32(uxLocalPort.Text));
+                sck.Bind(epLocal);
+
+                epRemote = new IPEndPoint(IPAddress.Parse(uxFriendIP.Text), Convert.ToInt32(uxFriendPort.Text));
+                sck.Connect(epRemote);
+
+                byte[] buffer = new byte[1500];
+                sck.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+
+                button3.Enabled = false;
+                button3.Text = "Connected";
+                uxSend.Enabled = true;
+                uxInputBox.Focus();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void UxSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+                byte[] msg = new byte[1500];
+                msg = enc.GetBytes(uxInputBox.Text);
+
+                sck.Send(msg);
+                uxTextBox.Text += "\nYou: " + uxInputBox.Text;
+                uxInputBox.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
         private void uxLogin_Click(object sender, EventArgs e)
